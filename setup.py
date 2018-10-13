95f3eaaddfe96a5a3dbbdb907c70a96c1bdc07dc
yzmac <728074993@qq.com>
2018-10-13 09:48:09 +0800

    0.1.70.1.70.1.70.1.70.1.70.1.70.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='linptech',
-    version='0.1.6',
+    version='0.1.7',
     description='Linptech serial protocol',
     author='yangzhan',
     author_email='728074993@qq.com',
